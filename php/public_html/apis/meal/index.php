55db2e8b211091eb5802397edd94739ea4dba937
CodeCanna <fleabass4@gmail.com>
2019-11-08 22:07:59 -0700

    Got data into RedisDB

--- php/public_html/apis/meal/index.php
+++ php/public_html/apis/meal/index.php
@@ -6,6 +6,9 @@ use CodeCann\Meal;
 use PHPUnit\Framework\InvalidArgumentException;
 use PHPUnit\Framework\MockObject\Stub\Exception;
 use Predis\Autoloader;
+use Prophecy\Argument\Token\ExactValueToken;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\Exception\InvalidUuidStringException;
 
 // Create empty reply
 $reply = new stdClass();
@@ -16,26 +19,35 @@ try {
     $method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
     // Create a new redis instance and connect to it
-    try {
-        $redis = new Predis\Client();
-        $redis->connect('127.0.0.1');
-        
-        // Check the connection by pinging the server
-        if ($redis->ping()) {
-            echo "Successfully connected to Redis!";
-        } else {
-            echo "Could not connect to Redis...";
-        }
-    } catch(Exception | TypeError | InvalidArgumentException $exception) {
-        $exceptionType = gettype($exception);
-        throw new $exceptionType($exception->getMessage());
+    $redis = new Predis\Client();
+    $redis->connect('127.0.0.1');
+
+    // Check the connection by pinging the server
+    if (!$redis->ping()) {
+        echo "Couldn't connect to Redis...";
     }
-    
 
-
-
+    // What to do if the reqeust is GET
     if ($method === 'GET') {
-        
+        try {
+            // Get value from redis
+            echo $redis->get($_GET['mealNameKey']);
+        } catch (Exception | TypeError | InvalidArgumentException | Predis\Response\ServerException $exception) {
+            $exceptionType = get_class($exception);
+            throw new $exceptionType($exception->getMessage());
+        }
+    // What to do if request is POST
+    } else if ($method === 'POST') {
+        try {
+            // Get POST values
+            foreach ($_POST as $meal) {
+                $redis->set(Uuid::uuid4(), $meal);
+            }
+            $redis->set('mealId', $_POST['mealId']);
+        } catch (Predis\Response\ServerException $exception) {
+            $exceptionType = get_class($exception);
+            throw (new $exceptionType($exception->getMessage()));
+        }
     }
 } catch (Exception $exception) {
     throw new $exception;
